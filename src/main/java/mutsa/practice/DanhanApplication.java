cef10f7a00f2e708cec490cbf050b75622032e34
danhandev <danhandev@gmail.com>
2025-03-27 19:22:01 +0900

    initial commit

---
+++ src/main/java/mutsa/practice/DanhanApplication.java
@@ -0,0 +1,13 @@
+package mutsa.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanhanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanhanApplication.class, args);
+	}
+
+}
